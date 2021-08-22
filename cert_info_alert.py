4d57adc0f3dd974711b6e82c75bd7d47053b517d
vnoook <noook@yandex.ru>
2021-08-22 23:28:51 +0700

    #10 писал в do_xlsx и processing_txt_files не рабочая

--- cert_info_alert.py
+++ cert_info_alert.py
@@ -16,6 +16,7 @@ dir_cers = r'cer_s'  # папка для сохранения сертифика
 dir_txts = r'txt_s'  # папка для дампов сертификатов
 # команда для командной строки windows = for /r cer_s %i in (*.cer) do certutil "%i" > "txt_s\%~ni.txt"
 cer_command = rf'for /r {dir_cers} %i in (*.cer) do certutil "%i" > "{dir_txts}\%~ni.txt"'
+name_file_xlsx = 'cert.xlsx'
 list_of_data_from_cert = []
 
 
@@ -39,22 +40,28 @@ def do_txt_from_cer():
 
 
 # функция чтения дампов сертификатов и формирования конечной таблицы для вывода её в xlsx
-def read_txt_files():
+def processing_txt_files():
     # Поля для поиска в файле
-    # дата начала 'NotBefore:'
-    # дата конца 'NotAfter:'
-    # организация выдавшая сертификат 'CN='
-    # отпечаток 'Хеш сертификата(sha1):'
-    # фамилия 'SN='
-    # имя отчество 'G='
-    # полный путь до сертификата  = os.path.abspath(data_of_scan)
-    # '' '' '' '' '' '' '' ''
+    dict_search_string = {1 : 'SN=',  # фамилия 'SN='
+                          2 : 'G=',  # имя отчество 'G='
+                          3 : 'CN=',  # организация выдавшая сертификат 'CN='
+                          4 : 'Хеш сертификата(sha1):',  # отпечаток 'Хеш сертификата(sha1):'
+                          5 : 'NotBefore:',  # дата начала 'NotBefore:'
+                          6 : 'NotAfter:',  # дата конца 'NotAfter:'
+                          7 : 'datetime.datetime.date(datetime.datetime.now())',  # текущая дата
+                          8 : 'os.path.abspath(data_of_scan)',  # полный путь до сертификата  = os.path.abspath(data_of_scan)
+                          9 : 'empty',
+                          10 : 'empty',
+                          11 : 'empty'
+                          }
     # переход в папку с дампами
     os.chdir(os.path.join(os.getcwd(), dir_txts))
     for data_of_scan in os.scandir():
         if data_of_scan.is_file() and os.path.splitext(os.path.split(data_of_scan)[1])[1] == etx_txt:
             txt_file = open(data_of_scan.name, 'r')
             list_of_lines = txt_file.readlines()
+
+            list_of_data_from_cert
 
             txt_file.close()
     print('(3)...дампы сертификатов прочитаны и таблица для записи в xlx готова')
@@ -65,13 +72,16 @@ def do_xlsx():
     # переход в корневую папку
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    name_file_xlsx = 'cert.xlsx'
+    # print(list_of_data_from_cert)
 
-    print(list_of_data_from_cert)
-
-    print(datetime.datetime.now())
+    print(datetime.datetime.date(datetime.datetime.now()))
 
     file_xlsx = openpyxl.Workbook()
+    file_xlsx_s = file_xlsx.active
+
+    # собираю строку по правилу выгрузки и добавляю её в файл
+    # file_xlsx_s.append([val1, val2, val3])
+
     file_xlsx.save(name_file_xlsx)
     file_xlsx.close()
     print('(4)...файл с датами собран')
@@ -81,7 +91,7 @@ def do_xlsx():
 if __name__ == '__main__':
     clean_dir_txts()
     do_txt_from_cer()
-    read_txt_files()
+    processing_txt_files()
     do_xlsx()
 
     print(f'сделано')
